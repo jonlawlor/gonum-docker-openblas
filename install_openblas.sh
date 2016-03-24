665d8217bc39dccced9337f886fb212178528636
Jonathan J Lawlor <jonathan.lawlor@gmail.com>
2016-03-24 15:41:53 -0400

    Create install_openblas.sh

---
+++ install_openblas.sh
@@ -0,0 +1,26 @@
+set -ex
+
+# fetch fortran to build OpenBLAS
+sudo apt-get install -qq gfortran
+
+# fetch OpenBLAS
+pushd ~
+sudo git clone --depth=1 git://github.com/xianyi/OpenBLAS
+
+
+# make OpenBLAS
+pushd OpenBLAS
+sudo make FC=gfortran -s
+sudo make PREFIX=/usr -s install
+popd
+
+# fetch cblas reference lib
+curl http://www.netlib.org/blas/blast-forum/cblas.tgz | tar -zx
+
+# make cblas and install
+pushd CBLAS
+sudo mv Makefile.LINUX Makefile.in
+sudo make BLLIB=/usr/lib/libopenblas.a alllib
+sudo mv lib/cblas_LINUX.a /usr/lib/libcblas.a
+popd
+popd
