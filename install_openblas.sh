595d9051c24551ca2abb1c56abcbd219b7caad0d
Jonathan J Lawlor <jonathan.lawlor@gmail.com>
2016-03-24 15:58:38 -0400

    Update install_openblas.sh

--- install_openblas.sh
+++ install_openblas.sh
@@ -1,17 +1,19 @@
 set -ex
 
 # fetch fortran to build OpenBLAS
-sudo apt-get install -y -qq git-core build-essential gfortran
+apt-get -y update
+apt-get -y install git-core build-essential gfortran
+
 
 # fetch OpenBLAS
 pushd ~
-sudo git clone --depth=1 git://github.com/xianyi/OpenBLAS
+git clone --depth=1 git://github.com/xianyi/OpenBLAS
 
 
 # make OpenBLAS
 pushd OpenBLAS
-sudo make FC=gfortran -s
-sudo make PREFIX=/usr -s install
+make FC=gfortran -s
+make PREFIX=/usr -s install
 popd
 
 # fetch cblas reference lib
@@ -19,9 +21,9 @@ curl http://www.netlib.org/blas/blast-forum/cblas.tgz | tar -zx
 
 # make cblas and install
 pushd CBLAS
-sudo mv Makefile.LINUX Makefile.in
-sudo make BLLIB=/usr/lib/libopenblas.a alllib
-sudo mv lib/cblas_LINUX.a /usr/lib/libcblas.a
+mv Makefile.LINUX Makefile.in
+make BLLIB=/usr/lib/libopenblas.a alllib
+mv lib/cblas_LINUX.a /usr/lib/libcblas.a
 popd
 popd
 
